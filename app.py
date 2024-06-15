d34091587e32d5df490aa7388eeaa76a46d926f1
kovendhan5 <kovendhanofficial5@gmail.com>
2024-06-15 15:28:05 +0530

    port change

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ def credentials_to_dict(credentials):
             'scopes': credentials.scopes}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=4000)
